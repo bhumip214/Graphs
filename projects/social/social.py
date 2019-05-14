7dddc1970081bd37b9768952d07a6423447e1e5b
Bhumi Patel <bhumip214@gmail.com>
2019-05-14 15:22:12 -0700

    completed populateGraph() and getAllSocialPaths()

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,8 +1,10 @@
-
+import random
 
 class User:
     def __init__(self, name):
         self.name = name
+    def __repr__(self):
+        return self.name
 
 class SocialGraph:
     def __init__(self):
@@ -31,6 +33,8 @@ class SocialGraph:
         self.friendships[self.lastID] = set()
 
     def populateGraph(self, numUsers, avgFriendships):
+    # Time Complexity: O(numUsers^2)
+    # Space Complexity: O(numUsers^2)
         """
         Takes a number of users and an average number of friendships
         as arguments
@@ -44,11 +48,36 @@ class SocialGraph:
         self.lastID = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
-        # Add users
+        # Add user
+        # Time Complexity: O(numUsers)
+        # Space Complexity: O(numUsers)
+        for i in range(numUsers):
+            self.addUser(f"User {i + 1}")
 
         # Create friendships
+        # avgFriendships = totalFriendships / numUsers
+        # totalFriendships = avgFriendships * numUsers
+        # Time Complexity: O(numUsers^2)
+        # Space Complexity: O(numUsers^2)
+        possibleFriendships = []
+        for userID in self.users:
+            for friendID in range(userID + 1, self.lastID + 1):
+                possibleFriendships.append((userID, friendID))
+        # print(possibleFriendships)
+
+        # Time Complexity: O(numUsers^2)
+        # Space Complexity: O(1)
+        random.shuffle(possibleFriendships)
+        # print(possibleFriendships)
+
+        # Time Complexity: O(avgFriendships * numUsers // 2)
+        # Space Complexity: O(avgFriendships * numUsers // 2)
+        for friendship_index in range(avgFriendships * numUsers // 2):
+            friendship = possibleFriendships[friendship_index]
+            # print(friendship)
+            self.addFriendship(friendship[0], friendship[1])
+
 
     def getAllSocialPaths(self, userID):
         """
@@ -59,14 +88,29 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
-        return visited
+        visited = {userID: [userID]}  # Note that this is a dictionary, not a set
+        friendship_network = []
+        friendship_network.append([userID])
 
+        while friendship_network:
+            path = friendship_network.pop()
+            #print(f"path {path}")
+            user = path[-1]
+            #print(f"user {user}")
+            
+            for friend in self.friendships[user]:
+                if friend not in visited:
+                    connected_path = path + [friend]
+                    #print(f"connected_path {connected_path}")
+                    visited[friend] = connected_path
+                    friendship_network.append(connected_path)
+                    #print(f"friendship_network {friendship_network}")
+        return visited
 
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populateGraph(10, 2)
+    # print(sg.users)
     print(sg.friendships)
     connections = sg.getAllSocialPaths(1)
     print(connections)
